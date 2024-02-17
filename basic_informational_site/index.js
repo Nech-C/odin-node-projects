0d933d4938ec9856b1011d2d6317e10112132aa3
Chen <xiaokunchen@outlook.com>
2024-02-17 10:51:41 -0500

    feat: impl with Express

--- basic_informational_site/index.js
+++ basic_informational_site/index.js
@@ -2,21 +2,31 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const express = require('express')
+const logger = require('morgan')
 
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url + '.html');
-    if (!fs.existsSync(filePath)) {
-        filePath = path.join(__dirname, '404.html');
-    }
 
-    fs.readFile(filePath, (err, data) => {
-        if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-    });
-});
- 
-
+const app = express()
 const PORT = 8080;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/:page', (req, res) => {
+    let filepath = path.join(__dirname, req.params.page);
+    filepath = filepath + '.html';
+    console.log(filepath);
+    if (fs.existsSync(filepath)) {
+        res.sendFile(filepath);
+    } else {
+        res.status(404).sendFile(path.join(__dirname, '404.html'));
+    }
+});
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
