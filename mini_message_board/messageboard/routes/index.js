23d88b9222c0c816f2c7efa03ca43725be618d14
Chen <xiaokunchen@outlook.com>
2024-02-23 19:51:23 -0500

    feat: complete assignment

--- mini_message_board/messageboard/routes/index.js
+++ mini_message_board/messageboard/routes/index.js
@@ -18,4 +18,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Mini Messageboard' });
+});
+
+
+router.post('/new', function(req, res, next) {
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date()
+  });
+  console.log(messages);
+  res.redirect('/');
+});
+
 module.exports = router;
