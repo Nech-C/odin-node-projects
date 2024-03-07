19cb531474e5e9fd6ccd1c30fd9c11b419c84622
Chen <xiaokunchen@outlook.com>
2024-03-06 22:18:36 -0500

    feat: impl genre creation routes

--- mdn_express_website/express-locallibrary-tutorial/models/genre.js
+++ mdn_express_website/express-locallibrary-tutorial/models/genre.js
@@ -1,5 +1,6 @@
 const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
+const {body, validationResult} = require('express-validator');
 
 const GenreSchema = new Schema({
     name: {type: String, required: true, min: 3, max: 100},
