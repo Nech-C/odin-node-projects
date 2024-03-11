47432ffc22e1534ec734f37709175d13c72b8308
Chen <xiaokunchen@outlook.com>
2024-03-11 15:37:57 -0400

    feat: impl genre delete

--- mdn_express_website/express-locallibrary-tutorial/controllers/genreController.js
+++ mdn_express_website/express-locallibrary-tutorial/controllers/genreController.js
@@ -83,12 +83,40 @@ exports.genre_create_post = [
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
+  const [genre, genreBooks] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Book.find({ genre: req.params.id }).exec(),
+  ]);
+  if (genre === null) {
+    res.redirect("/catalog/genres");
+  } else {
+    res.render("genre_delete", {
+      title: "Delete Genre",
+      genre: genre,
+      genre_books: genreBooks,
+    });
+  }
 });
 
 // Handle Genre delete on POST.
 exports.genre_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete POST");
+  console.log(req.body.genreid);
+  const [genre, genreBooks] = await Promise.all([
+    Genre.findById(req.body.genreid).exec(),
+    Book.find({ genre: req.body.genreid }).exec(),
+  ]);
+
+  if (genreBooks.length > 0) {
+    res.render("genre_delete", {
+      title: "Delete Genre",
+      genre: genre,
+      genre_books: genreBooks,
+    });
+    return;
+  } else {
+    await Genre.findByIdAndDelete(req.body.genreid).exec();
+    res.redirect("/catalog/genres");
+  }
 });
 
 // Display Genre update form on GET.
