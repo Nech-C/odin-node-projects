69395d3fb20ac9fb0349bb9e31ccce318ecfa3f1
Chen <xiaokunchen@outlook.com>
2024-07-25 22:53:27 -0400

    feat: add Register.jsx component

--- members_only/client/src/components/Register.jsx
+++ members_only/client/src/components/Register.jsx
@@ -1,29 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Register() {
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    const [error, setError] = useState('');
+
+    const handleChange = (e)  => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+        console.log(formData);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { username, email, password } = formData;
+        console.log('Registering user:', { username, email, password });
+        try {
+          const response = await axios.post('/api/register', { username, email, password});
+          console.log('User registered:', response.data);
+        } catch (error) {
+          console.error('Error registering user:', error);
+          setError('Registration failed');
+        }
+      };
+
     return (
         <div className="text-center py-10">
         <h1 className="text-3xl font-bold text-white">Create an Account</h1>
-        <form className="flex flex-col items-center">
-            <input
+        <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+        <input
             className="w-64 p-2 rounded m-2"
             type="text"
+            name="username"
+            value={formData.username}
             placeholder="Username"
-            />
-            <input
+            onChange={handleChange}
+            required
+        />
+        <input
+            className="w-64 p-2 rounded m-2"
+            type="email"
+            name="email"
+            value={formData.email}
+            placeholder="Email"
+            onChange={handleChange}
+            required
+        />
+        <input
             className="w-64 p-2 rounded m-2"
             type="password"
+            name="password"
+            value={formData.password}
             placeholder="Password"
-            />
-            <button className="bg-fuchsia-900 text-white py-2 px-4 rounded m-2">
-            REGISTER
-            </button>
-            <Link to="/login">
+            onChange={handleChange}
+            required
+        />
+        <button className="bg-fuchsia-900 text-white py-2 px-4 rounded m-2">
+        REGISTER
+        </button>
+        <Link to="/login">
             <button className="bg-fuchsia-900 text-white py-2 px-4 rounded m-2">
                 Back
             </button>
-            </Link>
+        </Link>
         </form>
         </div>
     );
