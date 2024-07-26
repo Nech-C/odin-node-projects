22434a18910677b11fda80b2fe712da57af0c2b8
Chen <xiaokunchen@outlook.com>
2024-07-25 22:54:32 -0400

    refactor: replace first_name and last_name with name in User schema

--- members_only/models/user.js
+++ members_only/models/user.js
@@ -4,19 +4,13 @@ const Schema = mongoose.Schema;
 
 
 const UserSchema = new Schema({
-    first_name: {type: String, required: true, maxLength: 100},
-    last_name: {type: String, required: true, maxLength: 100},
+    name: {type: String, required: true, maxLength: 100},
     email: {type: String, required: true, maxLength: 100},
     is_member: {type: Boolean, required: true},
     is_admin: {type: Boolean, required: true},
     password: {type: String, required: true, maxLength: 100},
     message_list: [{type: Schema.Types.ObjectId, ref: 'Message'}]
 });
-
-// Virtual for user's full name
-UserSchema.virtual('name').get( function() {
-    return this.first_name + ' ' + this.last_name;
-} );
 
 
 //Export model
