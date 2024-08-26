b9c3705d0a838bf4bb53862052da94cabd585742
Chen <xiaokunchen@outlook.com>
2024-08-26 12:31:01 -0400

    feat: add check-auth api

--- members_only/routes/api.js
+++ members_only/routes/api.js
@@ -9,4 +9,12 @@ router.get('/messages', messageController.getAllMessages);
 
 router.post('/messages', messageController.postMessage);
 
+router.get('/api/check-auth', (req, res) => {
+    if (req.isAuthenticated()) {
+      res.json({ isAuthenticated: true, user: req.user });
+    } else {
+      res.json({ isAuthenticated: false });
+    }
+  });
+
 module.exports = router;
