a1f7fc969894cb4759da6efaba1e9bb3d7f8b0c6
Chen <xiaokunchen@outlook.com>
2024-08-30 23:28:32 -0400

    feat: impl verifyToken middleware

---
+++ members_only/middleware/verifyToken.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const bearerHeader = req.headers['authorization'];
+  
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
+    
+    jwt.verify(bearerToken, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: 'Invalid token' });
+      }
+      req.user = decoded;
+      next();
+    });
+  } else {
+    res.status(401).json({ message: 'No token provided' });
+  }
+};
+
+module.exports = verifyToken;
