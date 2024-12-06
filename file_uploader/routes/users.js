1a821d01af654b9edac735b68acc0eaa34f81353
Nech-C <xiaokunchen@outlook.com>
2024-12-06 10:54:01 -0500

    upload current progress

--- file_uploader/routes/users.js
+++ file_uploader/routes/users.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.put('/new', function(req, res, next) {
+  try {
+    user = userController.createUser(req, res)
+  } catch (error) {
+    console.log(error)
+  }
+
+  if (user) {
+    res.send("User create!")
+  } else {
+    res.send("User creation failed.")
+  }
+})
 
 module.exports = router;
